b495bd840696cc9b51658891fdf59580331fea45
Doug Hodson <doug@openeaagles.org>
2018-07-09 19:58:44 -0400

    added a few scripts

---
+++ scripts/unicode-plot-example.jl
@@ -0,0 +1,9 @@
+#
+# to execute: julia 
+#
+using UnicodePlots
+lineplot([cos, sin], -pi/2, 2pi)
+
+using CSV
+case0101 = CSV.read("../sleuth-data/case0101.csv")
+intrinsic = filter(f->f[:Treatment] == "Intrinsic", case0101)
