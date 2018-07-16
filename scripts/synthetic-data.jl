306cfc37234dd0f68595bc2a435390ee0ead9d57
Doug Hodson <doug@openeaagles.org>
2018-07-15 20:16:34 -0400

    update

--- scripts/synthetic-data.jl
+++ scripts/synthetic-data.jl
@@ -1,7 +1,7 @@
 using Distributions
 srand(357)
-d = Normal()
-x1 = rand(d, 1000)
+d = Normal(0.0, 1.0)
+x1 = rand(d, 15)
 # descriptive stats
 mean(x1)
 std(x1)
@@ -11,11 +11,14 @@ fit(Normal, x1)
 
 using StatsBase
 summarystats(x1)
+# std error of the mean
+sem(x1)
 
 using StatPlots
 plot(d)
 plot(x1)
 
 using HypothesisTests
-t1 = OneSampleTTest(x1)
-t2 = OneSampleTTest(x1, 1.0)
+t1 = OneSampleTTest(x1, 0.0)
+t2 = OneSampleTTest(x1, 0.1)
+t3 = OneSampleTTest(x1, 0.4)
