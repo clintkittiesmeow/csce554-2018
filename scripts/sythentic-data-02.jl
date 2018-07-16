aade676e6e0306fc96d3d957796559b3fa5f898b
Doug Hodson <doug@openeaagles.org>
2018-07-16 10:31:03 -0400

    updated script

--- scripts/sythentic-data-02.jl
+++ scripts/sythentic-data-02.jl
@@ -2,8 +2,8 @@ using Distributions
 srand(357)
 d1 = Normal(0,1)
 d2 = Normal(5,1)
-x1 = rand(d1, 1000)
-x2 = rand(d2, 1000)
+x1 = rand(d1, 15)
+x2 = rand(d2, 15)
 # estimate parameters for each "sample"
 fit(Normal, x1)
 fit(Normal, x2)
