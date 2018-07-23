e7b6b64451bf0443176c0d8cf9ad95d38be393bc
Doug Hodson <doug@sidechannel.net>
2018-07-23 16:20:10 -0400

    updated script

--- scripts/multiple-plots.jl
+++ scripts/multiple-plots.jl
@@ -7,13 +7,13 @@ p4 = plot(rand(100), leg = false);
 plot(p1, p2, p3, p4)
 
 using Distributions;
+using StatPlots;
 srand(357);
 t1 = plot(TDist(5),  leg = false);
 t2 = plot(TDist(10), leg = false);
 t3 = plot(TDist(15), leg = false);
 t4 = plot(TDist(47), leg = false);
 
-using StatPlots;
 plot(t1, t2, t3, t4)
 params(TDist(5))
 dof(TDist(10))
@@ -35,4 +35,4 @@ F2 = pdf.(FDist(2, 1), x);
 F3 = pdf.(FDist(5, 2), x);
 F4 = pdf.(FDist(10, 1), x);
 F5 = pdf.(FDist(100, 100), x);
-plot(x, [F1, F2, F3, F4, F5], xlims=[0,5], xlims = [0,5], ylims=[0, 2.5])
+plot(x, [F1, F2, F3, F4, F5], xlims = [0,5], ylims=[0, 2.5])
