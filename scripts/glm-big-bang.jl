eda11d39e40e98205b4f7464f96b14ce8c9a273f
Doug Hodson <doug@openeaagles.org>
2018-07-25 10:02:02 -0400

    updated script

--- scripts/glm-big-bang.jl
+++ scripts/glm-big-bang.jl
@@ -2,6 +2,8 @@
 using CSV;
 case0701 = CSV.read("./sleuth-data/case0701.csv");
 names(case0701)
+
+
 using GLM;
 x = lm(@formula(Distance ~ Velocity), case0701)
 y = lm(@formula(Distance ~ Velocity - 1), case0701)
