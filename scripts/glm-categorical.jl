1df3502193e1db13ed58ef7bfae7f981b8c781dc
Doug Hodson <doug@openeaagles.org>
2018-08-14 21:32:45 -0400

    update

--- scripts/glm-categorical.jl
+++ scripts/glm-categorical.jl
@@ -1,14 +1,19 @@
 using CSV;
 using DataFrames
-using GLM;
 
 case0101 = CSV.read("./sleuth-data/case0101.csv");
-#names(case0101)
-#showall(case0101)
 describe(case0101)
 
-categorical!(case0101, :Treatment);
-levels(case0101[:Treatment])
+using GLM;
+x = lm(@formula(Score ~ Treatment), case0101)
 
-case1002 = CSV.read("./sleuth-data/case1002.csv");
-describe(case1002)
+x = lm(@formula(Score ~ Treatment-1), case0101)
+
+using HypothesisTests;
+intrinsic = filter(f -> f[:Treatment] == "Intrinsic", case0101);
+extrinsic = filter(f -> f[:Treatment] == "Extrinsic", case0101);
+
+intrinsic_arr = convert(Array{Float64}, intrinsic[:, 1]);
+extrinsic_arr = convert(Array{Float64}, extrinsic[:, 1]);
+
+test = EqualVarianceTTest(intrinsic_arr, extrinsic_arr)
