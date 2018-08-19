d7bbde18deb4a62e10dbd4675cd2050842c6043a
Doug Hodson <doug@sidechannel.net>
2018-08-19 11:23:47 -0400

    update

--- scripts/doe-ch1.jl
+++ scripts/doe-ch1.jl
@@ -2,12 +2,16 @@
 using DataFrames
 df = DataFrame(ball   = ["B", "B", "B", "B", "T", "T", "T", "T"],
                driver = ["O", "O", "R", "R", "O", "O", "R", "R"],
-               score  = [88,   90,  93,  91,  88,  91,  92,  94])
-categorical!(df, :ball);
-categorical!(df, :driver);
+               score  = [88,   90,  93,  91,  88,  91,  92,  94]);
+categorical!(df, [:ball, :driver]);
 describe(df)
 
+using StatsModels
 using GLM
-x = lm(@formula(score ~ driver), df)
-x = lm(@formula(score ~ ball), df)
-x = lm(@formula(score ~ ball*driver), df)
+lm1 = lm(@formula(score ~ driver), df)
+lm2 = lm(@formula(score ~ ball), df)
+f = @formula(score ~ ball*driver)
+f = dropterm(f, :ball)
+f = dropterm(f, :driver)
+lm3 = lm(@formula(score ~ +(ball&driver)), df)
+lm4 = lm(@formula(score ~ ball*driver), df)
